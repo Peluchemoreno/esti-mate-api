5b3614fbaabb10e021a8a4c9997be0bb2d94d79a
justin <justin@sharktub.v2>
2025-09-01 10:40:34 -0500

    add catch for mongodburi

--- app.js
+++ app.js
@@ -14,17 +14,18 @@ const mongoose = require("mongoose");
 const mainRouter = require("./routes/index");
 
 const app = express();
-app.get("/health", (req, res) => {
-  res.status(200).send("ok");
-});
 
 /* mongoose.connect(dataBase, () => {
   console.log("connected successfully to db");
 });
  */
-mongoose.connect(process.env.MONGODB_URI, {
-  dbName: process.env.MONGO_DB || "app",
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    `mongodb+srv://jmcdmoreno19:tacobell22@testingcluster.rsp5krz.mongodb.net/?retryWrites=true&w=majority&appName=TestingCluster`,
+  {
+    dbName: process.env.MONGO_DB || "app",
+  }
+);
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 // app.use(cors());
 app.use(express.json({ limit: "150mb" }));
