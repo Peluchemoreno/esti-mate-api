e27c0708dc6c148f4f813b8bb376e8685405004b
justin <justin@sharktub.v2>
2025-09-27 14:54:13 -0500

    import jwt module

--- routes/stripe.js
+++ routes/stripe.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
-
+const jwt = require("jsonwebtoken");
 const Stripe = require("stripe");
 const mongoose = require("mongoose");
 const authorize = require("../middlewares/auth"); // your Bearer auth middleware
