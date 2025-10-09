f461dbce1f4469af6ce05a6b42784bec761c28df
justin <justin@sharktub.v2>
2025-10-09 15:57:05 -0500

    testing prod

--- scripts/reEnsureUserCatalog.js
+++ scripts/reEnsureUserCatalog.js
@@ -2,14 +2,19 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const { ensureUserCatalog } = require("../services/productCopyService");
+import User from "../models/user";
 
 const USER_ID = "68e8125e132f162dd9bd3b9f";
+
 (async () => {
   await mongoose.connect(process.env.MONGODB_URI, {
     dbName: process.env.MONGO_DB || "esti-mate",
   });
   console.log("Connected");
-  await ensureUserCatalog(USER_ID);
+  const users = await User.find({});
+  console.log(users);
+
+  // await ensureUserCatalog(USER_ID);
   console.log("Done");
   process.exit(0);
 })();
