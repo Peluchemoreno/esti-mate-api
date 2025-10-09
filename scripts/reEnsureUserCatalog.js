b5a92d1742ba42d660ee7eeaa6aca8812513b69a
justin <justin@sharktub.v2>
2025-10-09 15:12:38 -0500

    trying product fix again

---
+++ scripts/reEnsureUserCatalog.js
@@ -0,0 +1,15 @@
+// scripts/bootstrap_user_catalog.js
+require("dotenv").config();
+const mongoose = require("mongoose");
+const { ensureUserCatalog } = require("../services/productCopyService");
+
+const USER_ID = "68e8125e132f162dd9bd3b9f";
+(async () => {
+  await mongoose.connect(process.env.MONGODB_URI, {
+    dbName: process.env.MONGO_DB || "esti-mate",
+  });
+  console.log("Connected");
+  await ensureUserCatalog(USER_ID);
+  console.log("Done");
+  process.exit(0);
+})();
